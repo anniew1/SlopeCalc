5a965f7693c8559b963646122d3dfde837dcc999
BT_1N3_23 <bths@sample.bths>
2023-10-17 09:31:16 -0400

    logic

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class LinearEquation {
+public class LinearEquation {
+
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+
+    public LinearEquation(int x1, int y1, int x2, int y2){
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    private double distance(){
+        return Math.sqrt(Math.pow((x2-x1), 2.0) + Math.pow((y2-y1), 2.0));
+    }
+
+    private double slope(){
+        return (y2-y1)/(x2-x1);
+    }
+
+    private double yIntercept() {
+        return y1 - slope() * x1;
+    }
+
+    private String equation(){
+        return "y = " + slope() + yIntercept();
+    }
+
+    private String coordinateForX(double x) {
+        return ("(" + (x * slope()) + "+" + yIntercept() + ")");
+    }
+
+    public String lineInfo(){
+        String info = "";
+        info += "The two points are: " + "(" + x1 + ", " + y1 + ") and " + "(" + x2 + ", " + y2 + ")";
+        info += "\nThe equation of the line between these points is " + equation();
+        info += "\nThe slope of this line is: " + slope();
+        info += "\nThe y-intercept of this line is: " + yIntercept();
+        info += "\nThe distance between the two points is: 12.65";
+        return info;
+    }
+
+
+
+
+
 }
--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class LinearEquationLogic {
+import java.util.Scanner;
+public class LinearEquationLogic {
+
+    private String coord1;
+    private String coord2;
+    private Scanner scan;
+
+    private LinearEquation linear;
+
+
+    public LinearEquationLogic(){
+        coord1 = "(0, 0)";
+        coord2 = "(0, 0)";
+        scan = new Scanner(System.in);
+    }
+
+    public void start(){
+        getCoords();
+        menu();
+    }
+
+    private void getCoords(){
+        System.out.println("Welcome to the linear equation calculator");
+        System.out.print("Enter coordinate 1: ");
+        coord1 = scan.nextLine();
+        System.out.print("Enter coordinate 2: ");
+        coord2 = scan.nextLine();
+
+        int commaidx = coord1.indexOf(",");
+        int x1 = Integer.parseInt(coord1.substring(1, commaidx));
+        int y1 = Integer.parseInt(coord1.substring(commaidx + 1, coord1.length()-1));
+
+        int commaidx2 = coord2.indexOf(",");
+        int x2 = Integer.parseInt(coord2.substring(1, commaidx2));
+        int y2 = Integer.parseInt(coord2.substring(commaidx2 + 1, coord1.length()-1));
+
+        linear = new LinearEquation(x1, y1, x2, x2);
+
+        }
+    private void menu(){
+        String option = "y";
+        while (option.equals("y")){
+            System.out.println("");
+            linear.lineInfo();
+
+        }
+
+
+    }
+
+
+
+
 }
